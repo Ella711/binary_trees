bfd59e5cb515a217471cc25189d3af5a2beff9ed
Byakko12 <davidlandg@gmail.com>
2022-03-23 11:46:51 -0500

    [FIX] - 10 recursion

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,15 +9,8 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
-
     if (!tree)
-        return(0);
-    while (tree->parent)
-    {
-        tree = tree->parent;
-        depth++;
-    }
-    
-    return (depth);
+        return (-1);
+
+    return (binary_tree_depth(tree->parent) + 1);
 }
