6a16f7daad39c7e13bc93c9e14b78df9d72260bc
Ella711 <allenro711@gmail.com>
2022-03-23 15:36:38 -0500

    [FIX] Styling

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,12 +9,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	
+
 	if (!tree->right && !tree->left)
 		return (1);
-	
+
 	if (tree->right && tree->left)
 		return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
-	
-	return 0;
+
+	return (0);
 }
+
