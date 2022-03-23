4088ae0ec1e4046ada662d599afc73198755ebcc
Byakko12 <davidlandg@gmail.com>
2022-03-23 13:16:10 -0500

    [FIX] - test conditionals

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,7 +19,7 @@ int max(int num1, int num2)
  */
 size_t recursive_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	return (max(recursive_height(tree->left), recursive_height(tree->right) + 1));
@@ -32,9 +32,8 @@ size_t recursive_height(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	return (recursive_height(tree) - 1);
 }
-
