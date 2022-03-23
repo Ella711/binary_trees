8ec38efdac91aed98ab3667bb3fffb7e6300bb49
Byakko12 <davidlandg@gmail.com>
2022-03-23 13:03:46 -0500

    [FIX] - identation file 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,16 +1,27 @@
 #include "binary_trees.h"
-
+/**
+ * recursive_size - recursive function to find size
+ *
+ * @tree: node type binary_tree_t
+ * Return: size of a binary tree
+ */
 size_t recursive_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    return recursive_size(tree->left) + recursive_size(tree->right) + 1;
+	return (recursive_size(tree->left) + recursive_size(tree->right) + 1);
 }
+/**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: node type binary_tree_t
+ * Return: size of a binary tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return(0);
+	if (!tree)
+		return (0);
 
-    return (recursive_size(tree));
+	return (recursive_size(tree));
 }
