f729c670076b34f16b56c106aed87a515937090d
Ella711 <allenro711@gmail.com>
2022-03-23 12:15:14 -0500

    [FIX] Conditionals

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,29 +1,36 @@
 #include "binary_trees.h"
 /**
- * @brief
+ * max - max number
  *
- * @param num1
- * @param num2
- * @return int
+ * @num1: number one
+ * @num2: number two
+ * Return: max number
  */
 int max(int num1, int num2)
 {
 	return ((num1 > num2) ? num1 : num2);
 }
+
+/**
+ * recursive_height - measures the height of a binary tree
+ *
+ * @tree: node type binary_tree_t
+ * Return: height of a binary tree
+ */
+size_t recursive_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (max(recursive_height(tree->left), recursive_height(tree->right) + 1));
+}
+
 /**
  * binary_tree_height - measures the height of a binary tree
  *
  * @tree: node type binary_tree_t
  * Return: height of a binary tree
  */
-
-size_t recursive_height(const binary_tree_t *tree)
-{
-	if (!tree || (!tree->left && !tree->right))
-		return (-1);
-
-	return (max(recursive_height(tree->left), recursive_height(tree->right) + 1));
-}
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
