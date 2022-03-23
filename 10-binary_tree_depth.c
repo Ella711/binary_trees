d8c82c6bb6736ff16d8d067c0fab731fe6d66909
Ella711 <allenro711@gmail.com>
2022-03-23 12:30:22 -0500

    [FIX] return statement

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -23,12 +23,9 @@ size_t recursion_tree_depth(const binary_tree_t *tree)
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t result = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	result = recursion_tree_depth(tree);
-	return (result - 1);
+	return (recursion_tree_depth(tree) - 1);
 }
 
