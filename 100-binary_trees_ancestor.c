a2080259c0babcbc5ea1eb317fb7a640f5f9ece1
Byakko12 <davidlandg@gmail.com>
2022-03-24 13:55:10 -0500

    [FIX] - identation of files

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,10 +9,10 @@
 
 size_t recursion_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (recursion_tree_depth(tree->parent) + 1);
+	return (recursion_tree_depth(tree->parent) + 1);
 }
 /**
  * binary_tree_depth - measures the depth of a binary tree
@@ -23,29 +23,31 @@ size_t recursion_tree_depth(const binary_tree_t *tree)
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (recursion_tree_depth(tree) - 1);
+	return (recursion_tree_depth(tree) - 1);
 }
 /**
  * binary_trees_ancestor -  finds the lowest common ancestor of two nodes
  *
- * @tree: node type binary_tree_t
+ * @first: node type binary_tree_t
+ * @second: node type binary_tree_t
  * Return: lowest common ancestor of two nodes
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
-    if (!first->parent || !second->parent || !second || !first)
-        return (NULL);
+	if (!first->parent || !second->parent || !second || !first)
+		return (NULL);
 
-    if (first->parent == second->parent || second == first->parent)
-        return (first->parent);
-    if (first == second->parent)
-        return (second->parent);
-    else if (binary_tree_depth(first) >= binary_tree_depth(second))
-        binary_trees_ancestor(first->parent, second);
-    else
-        binary_trees_ancestor(first, second->parent);
-    return(first->parent);
+	if (first->parent == second->parent || second == first->parent)
+		return (first->parent);
+	if (first == second->parent)
+		return (second->parent);
+	else if (binary_tree_depth(first) >= binary_tree_depth(second))
+		binary_trees_ancestor(first->parent, second);
+	else
+		binary_trees_ancestor(first, second->parent);
+	return (first->parent);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -24,11 +24,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *uncle = NULL;
-	
+
 	if (!node || !node->parent)
 		return (NULL);
-	
+
 	uncle = binary_tree_sibling(node->parent);
-	
+
 	return (uncle);
 }
