9c4a1f59f57fa55d11a93647e3b8cd4f3f6e97e9
Byakko12 <davidlandg@gmail.com>
2022-03-24 14:59:44 -0500

    [FIX] - return of file 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -38,7 +38,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second)
 {
-	if (!first->parent || !second->parent)
+	if (!first->parent || !second->parent || !first || !second)
 		return (NULL);
 
 	if (first->parent == second->parent || second == first->parent)
@@ -46,8 +46,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (first == second->parent)
 		return (second->parent);
 	else if (binary_tree_depth(first) >= binary_tree_depth(second))
-		binary_trees_ancestor(first->parent, second);
+		return (binary_trees_ancestor(first->parent, second));
 	else
-		binary_trees_ancestor(first, second->parent);
-	return (first->parent);
+		return (binary_trees_ancestor(first, second->parent));
+	
 }
